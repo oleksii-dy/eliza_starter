d0616ed8fdf0dc3f89e4139f349e2b2d8a44328e
Blair Lee <iam@blairlee.me>
2024-12-26 13:31:41 +0900

    mod: fix node version on integration test to 23.3.0

--- scripts/integrationTests.sh
+++ scripts/integrationTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check Node.js version
-REQUIRED_NODE_VERSION=23
+REQUIRED_NODE_VERSION=23.3.0
 CURRENT_NODE_VERSION=$(node -v | cut -d'.' -f1 | sed 's/v//')
 
 if (( CURRENT_NODE_VERSION < REQUIRED_NODE_VERSION )); then
