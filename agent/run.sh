4e0c3bcc0375ebf183ed8acb354d9bce7fbf0206
mike dupont <mike.dupont@introspector.local>
2025-01-04 08:02:19 -0500

    moving code to telemetry

--- agent/run.sh
+++ agent/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# --trace-event-categories node,node.bootstrap,node.console,node.vm.script,v8,node.http,node.net.native,node.environment   
-node --enable-source-maps --heap-prof --expose-gc  --prof --cpu-prof --loader ts-node/esm src/index.ts "--isRoot" "--characters=characters/eliza.character.json"
+# 
+#node --trace-event-categories node,node.bootstrap,node.console,node.vm.script,v8,node.http,node.net.native,node.environment    --enable-source-maps --heap-prof --expose-gc  --prof --cpu-prof --loader ts-node/esm src/index.ts "--isRoot" "--characters=characters/eliza.character.json"
+
+#node --import telemetry.js --trace-event-categories node,node.bootstrap,node.console,node.vm.script,v8,node.http,node.net.native,node.environment    --enable-source-maps --heap-prof --expose-gc  --prof --cpu-prof --loader ts-node/esm src/index.ts "--isRoot" "--characters=characters/eliza.character.json"
+#strace -f -o str
+node  --trace-event-categories node,node.bootstrap,node.console,node.vm.script,v8,node.http,node.net.native,node.environment    --enable-source-maps --heap-prof --expose-gc  --prof --cpu-prof --loader ts-node/esm src/index.ts "--isRoot" "--characters=characters/eliza.character.json"
