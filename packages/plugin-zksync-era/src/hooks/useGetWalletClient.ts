bff02fe7280ee8f46e294158060c08dab1d252af
aalimsahin <alimsahin0007@gmail.com>
2025-01-04 14:30:12 +0300

    feat: add hooks and constants

---
+++ packages/plugin-zksync-era/src/hooks/useGetWalletClient.ts
@@ -0,0 +1,10 @@
+import { createWalletClient, http } from "viem";
+import { zksync } from "viem/chains";
+import { eip712WalletActions } from "viem/zksync";
+
+export const useGetWalletClient = (): ReturnType<typeof createWalletClient> => {
+    return createWalletClient({
+        chain: zksync,
+        transport: http(),
+    }).extend(eip712WalletActions());
+};
