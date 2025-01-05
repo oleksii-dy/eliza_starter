d3b1b25c2ff2939db8bf79d9ca638e0cb985765a
mike dupont <mike.dupont@introspector.local>
2025-01-04 19:35:46 -0500

    fixing the agent name

--- packages/plugin-node/scripts/postinstall.js
+++ packages/plugin-node/scripts/postinstall.js
@@ -1,18 +1,18 @@
-import os from "os";
-import { elizaLogger } from "@elizaos/core";
-const platform = os.platform();
+// import os from "os";
+// import { elizaLogger } from "@elizaos/core";
+// const platform = os.platform();
 
-if (
-    platform === "linux" &&
-    !(os.release().includes("ubuntu") || os.release().includes("debian"))
-) {
-    elizaLogger.log(
-        "Skipping playwright installation on unsupported platform:",
-        platform
-    );
-} else {
-    const { execSync } = await import("child_process");
-    execSync("npx playwright install-deps && npx playwright install", {
-        stdio: "inherit",
-    });
-}
+// if (
+//     platform === "linux" &&
+//     !(os.release().includes("ubuntu") || os.release().includes("debian"))
+// ) {
+//     elizaLogger.log(
+//         "Skipping playwright installation on unsupported platform:",
+//         platform
+//     );
+// } else {
+//     const { execSync } = await import("child_process");
+//     execSync("npx playwright install-deps && npx playwright install", {
+//         stdio: "inherit",
+//     });
+// }
