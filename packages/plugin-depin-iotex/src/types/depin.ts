20d647ec8449502eae982f671d7d4de78430dfda
Nikita Ruban <64008830+nicky-ru@users.noreply.github.com>
2025-01-05 13:51:26 +0000

    chore: add weather data type

--- packages/plugin-depin-iotex/src/types/depin.ts
+++ packages/plugin-depin-iotex/src/types/depin.ts
@@ -21,3 +21,34 @@ export type DepinScanProject = {
     coingecko_id: string;
     fully_diluted_valuation: string;
 };
+
+export type WeatherData = {
+    latitude: number;
+    longitude: number;
+    temperature: number;
+    condition: string;
+    condition_desc: string;
+    condition_code: number;
+    temperature_min: number;
+    temperature_max: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+    wind_speed: number;
+    wind_scale: number;
+    wind_direction: number;
+    uv: number;
+    luminance: number;
+    elevation: number;
+    rain: number;
+    wet_bulb: number;
+    timestamp: number;
+    parsed_timestamp: string;
+    timezone: number;
+    location_name: string;
+    address: string;
+    source: string;
+    tag: string;
+    is_online: boolean;
+    is_malfunction: boolean;
+}
--- packages/plugin-depin-iotex/src/utils/weather.ts
+++ packages/plugin-depin-iotex/src/utils/weather.ts
@@ -1,17 +1,22 @@
 import { IAgentRuntime } from "@elizaos/core";
 import axios from "axios";
 
+import { WeatherData } from "../types/depin";
+
 export async function getWeather(
     runtime: IAgentRuntime,
     coordinates: { lat: number; lon: number }
-) {
+): Promise<WeatherData> {
     const apiKey = runtime.getSetting("NUBILA_API_KEY");
     const apiUrl = `https://api.nubila.ai/api/v1/weather?lat=${coordinates.lat}&lon=${coordinates.lon}`;
     const response = await axios.get(apiUrl, {
         headers: { "x-api-key": apiKey },
     });
     if (response.data.ok) {
-        return response.data;
+        return {
+            ...response.data.data,
+            parsed_timestamp: new Date(response.data.data.timestamp * 1000).toISOString()
+        };
     } else {
         throw new Error("Failed to fetch weather data");
     }
