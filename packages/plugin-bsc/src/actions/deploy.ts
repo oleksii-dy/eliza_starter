e039fa11b40ef49afede1c4f83e32ed2e78c8e41
ybgbob <bob.g@nodereal.io>
2025-01-07 01:09:36 +0800

    chore: DeployAction

--- packages/plugin-bsc/src/actions/deploy.ts
+++ packages/plugin-bsc/src/actions/deploy.ts
@@ -17,93 +17,6 @@ export { ercContractTemplate };
 
 export class DeployAction {
     constructor(private walletProvider: WalletProvider) {}
-
-    // async getBalance(params: GetBalanceParams): Promise<GetBalanceResponse> {
-    //     let { chain, address, token } = params;
-
-    //     elizaLogger.log("params", params);
-    //     if (chain == "bscTestnet") {
-    //         throw new Error("Testnet is not supported");
-    //     }
-
-    //     if (!address) {
-    //         address = this.walletProvider.getAddress();
-    //     }
-
-    //     this.walletProvider.switchChain(chain);
-    //     const nativeSymbol =
-    //         this.walletProvider.getChainConfigs(chain).nativeCurrency.symbol;
-    //     const chainId = this.walletProvider.getChainConfigs(chain).id;
-
-    //     try {
-    //         // If specific token is requested and it's not the native token
-    //         if (token && token !== nativeSymbol) {
-    //             const balance = await this.getTokenBalance(
-    //                 chainId,
-    //                 address,
-    //                 token
-    //             );
-    //             return {
-    //                 chain,
-    //                 address,
-    //                 balances: [{ token, balance }],
-    //             };
-    //         }
-
-    //         // If no specific token is requested, get all token balances
-    //         if (!token) {
-    //             const balances = await this.getTokenBalances(chainId, address);
-    //             return {
-    //                 chain,
-    //                 address,
-    //                 balances,
-    //             };
-    //         }
-
-    //         // If native token is requested
-    //         const nativeBalanceWei = await this.walletProvider
-    //             .getPublicClient(chain)
-    //             .getBalance({ address });
-    //         return {
-    //             chain,
-    //             address,
-    //             balances: [
-    //                 {
-    //                     token: nativeSymbol,
-    //                     balance: formatEther(nativeBalanceWei),
-    //                 },
-    //             ],
-    //         };
-    //     } catch (error) {
-    //         throw new Error(`Get balance failed: ${error.message}`);
-    //     }
-    // }
-
-    // async getTokenBalance(
-    //     chainId: ChainId,
-    //     address: Address,
-    //     tokenSymbol: string
-    // ): Promise<string> {
-    //     const token = await getToken(chainId, tokenSymbol);
-    //     const tokenBalance = await getTokenBalance(address, token);
-    //     return formatUnits(tokenBalance?.amount ?? 0n, token.decimals);
-    // }
-
-    // async getTokenBalances(
-    //     chainId: ChainId,
-    //     address: Address
-    // ): Promise<Balance[]> {
-    //     const tokensResponse = await getTokens();
-    //     const tokens = tokensResponse.tokens[chainId];
-
-    //     const tokenBalances = await getTokenBalances(address, tokens);
-    //     return tokenBalances
-    //         .filter((balance) => balance.amount && balance.amount !== 0n)
-    //         .map((balance) => ({
-    //             token: balance.symbol,
-    //             balance: formatUnits(balance.amount!, balance.decimals),
-    //         }));
-    // }
 }
 
 export const deployAction = {
@@ -136,7 +49,6 @@ export const deployAction = {
             modelClass: ModelClass.LARGE,
         });
 
-        elizaLogger.log("content", typeof content);
         elizaLogger.log("content", content);
 
         const privateKey = runtime.getSetting("BSC_PRIVATE_KEY");
@@ -198,46 +110,19 @@ export const deployAction = {
             elizaLogger.log("result: ", result);
         }
 
-        callback?.({
-            text: `Successfully create contract - ${result?.address}`,
-            content: { ...result },
-        });
-
-        // const deployOptions: DeployParams = {
-        //     chain: content.chain,
-        //     name: content.name,
-        //     contractType: content.contractType,
-        //     decimals: content.decimals,
-        //     symbol: content.symbol,
-        //     totalSupply: content.totalSupply,
-        // };
-
-        // const walletProvider = initWalletProvider(runtime);
-        // const action = new DeployAction(walletProvider);
-
-        // const {
-        //     contractType,
-        //     name,
-        //     symbol,
-        //     network,
-        //     baseURI,
-        //     totalSupply,
-        // } = contractDetails.object;
+        if (result) {
+            callback?.({
+                text: `Successfully create contract - ${result?.address}`,
+                content: { ...result },
+            });
+        } else {
+            callback?.({
+                text: `Unsuccessfully create contract`,
+                content: { ...result },
+            });
+        }
 
         try {
-            // const getBalanceResp = await action.getBalance(getBalanceOptions);
-            // if (callback) {
-            //     let text = `No balance found for ${getBalanceOptions.address} on ${getBalanceOptions.chain}`;
-            //     if (getBalanceResp.balances.length > 0) {
-            //         text = `Balance of ${getBalanceResp.address} on ${getBalanceResp.chain}:\n${getBalanceResp.balances
-            //             .map(({ token, balance }) => `${token}: ${balance}`)
-            //             .join("\n")}`;
-            //     }
-            //     callback({
-            //         text,
-            //         content: { ...getBalanceResp },
-            //     });
-            // }
             return true;
         } catch (error) {
             elizaLogger.error("Error in get balance:", error.message);
