5be7eb4489193ea5a6c95975fe59f61bcb71168a
goalongway <goalongway@163.com>
2025-01-09 07:42:06 +0800

    modify b2 plugin

--- packages/plugin-b2/src/plugins/index.ts
+++ packages/plugin-b2/src/plugins/index.ts
@@ -3,7 +3,7 @@ import { transferAction } from "../actions/transfer";
 import { walletProvider } from "../providers";
 
 export const b2Plugin: Plugin = {
-    name: "b2 plugin",
+    name: "b2",
     description: "B2 Network Plugin for Eliza",
     actions: [transferAction],
     providers: [walletProvider],
