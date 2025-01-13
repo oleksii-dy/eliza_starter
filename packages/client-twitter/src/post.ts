fc6a26d2fc6169769f7d7cec888c6db88990496c
Ricardo Ferrari <rferrariwd@gmail.com>
2025-01-13 01:34:09 -0300

    update: client twitter

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -45,8 +45,10 @@ const twitterPostTemplate = `
 
 {{postDirections}}
 
+REMEMBER: DO NOT ADD COMMENTARY OR ACKNOWLEDGE THIS REQUEST, JUST WRITE THE POST.
+
 # Task: Generate a post in the voice and style and perspective of {{agentName}} @{{twitterUserName}}.
-Write a post that is {{adjective}} about {{topic}} (without mentioning {{topic}} directly), from the perspective of {{agentName}}. Do not add commentary or acknowledge this request, just write the post.
+Write a post that is {{adjective}} about {{topic}} (without mentioning {{topic}} directly), from the perspective of {{agentName}}.
 Your response should be 1, 2, or 3 sentences (choose the length at random).
 Your response should not contain any questions. Brief, concise statements only. The total character count MUST be less than {{maxTweetLength}}. No emojis. Use \\n\\n (double spaces) between statements if there are multiple statements in your response.`;
 
