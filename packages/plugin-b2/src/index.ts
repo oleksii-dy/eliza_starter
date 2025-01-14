daf7ac9d0e7819b6d8072a43c1867325a80a09db
Afanti Afanti <phpjavaphp@gmail.com>
2025-01-14 00:02:34 +0000

    fix: default empty value and resolve type error

--- packages/plugin-b2/src/index.ts
+++ packages/plugin-b2/src/index.ts
@@ -1,5 +1,18 @@
-import { b2Plugin } from "./plugins";
+import { Plugin } from "@elizaos/core";
+import { transferAction } from "./actions/transfer";
+import { stakeAction } from "./actions/stake";
+import { unstakeAction } from "./actions/unstake";
+import { withdrawAction } from "./actions/withdraw";
+import { walletProvider } from "./providers";
 
-export * from "./plugins";
+const b2Plugin: Plugin = {
+    name: "b2",
+    description: "B2 Network Plugin for Eliza",
+    actions: [transferAction, stakeAction, unstakeAction, withdrawAction],
+    providers: [walletProvider],
+    evaluators: [],
+    services: [],
+    clients: [],
+};
 
 export default b2Plugin;
