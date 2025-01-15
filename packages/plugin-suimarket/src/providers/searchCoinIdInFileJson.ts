d1cdf41e7de1618e0c63ce7a79a577192f388388
Duc LA <laminhduc2610@gmail.com>
2025-01-15 11:08:50 +0700

    update tokenInfo

--- packages/plugin-suimarket/src/providers/searchCoinIdInFileJson.ts
+++ packages/plugin-suimarket/src/providers/searchCoinIdInFileJson.ts
@@ -1,18 +1,23 @@
 
 import fs from "fs/promises"
+import { fileURLToPath } from "url";
+import path from "path";
 
-
+// Chuyển đổi import.meta.url sang __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const files = [
-    "../coin_list/coin_list_part_1.json",
-    "../coin_list/coin_list_part_2.json",
-    "../coin_list/coin_list_part_3.json",
-    "../coin_list/coin_list_part_4.json",
-  ];
+    path.join(__dirname, "../src/coin_list/coin_list_part_1.json"),
+    path.join(__dirname, "../src/coin_list/coin_list_part_2.json"),
+    path.join(__dirname, "../src/coin_list/coin_list_part_3.json"),
+    path.join(__dirname, "../src/coin_list/coin_list_part_4.json"),
+];
 
 const searchCoinInFIleJsonProvider = async(coinSymbol:string, coinName:string)=>{
     const results = await Promise.all(
         files.map(async (file) => {
             try {
+
                 const data = await fs.readFile(file, 'utf8');
                 const coins = JSON.parse(data);
 
