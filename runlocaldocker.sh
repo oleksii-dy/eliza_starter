87acab2272421327f7fc7c973a21e3e82da43965
mike dupont <mike.dupont@introspector.local>
2025-01-15 09:48:45 -0500

    runlocaldocker

--- runlocaldocker.sh
+++ runlocaldocker.sh
@@ -1,2 +1,6 @@
 #
+/usr/bin/bash -c 'docker login -u AWS -p $(aws ecr get-login-password --region us-east-2) 767503528736.dkr.ecr.us-east-2.amazonaws.com'
+
+/usr/bin/docker pull 767503528736.dkr.ecr.us-east-2.amazonaws.com/agent/eliza:feature-arm64_fastembed
+
 /usr/bin/docker run -p 3000:3000 --mount type=bind,source=/opt/agent,target=/opt/agent --env-file /var/run/agent/secrets/env  --rm --name "agent-docker.service" 767503528736.dkr.ecr.us-east-2.amazonaws.com/agent/eliza:latest pnpm start:debug --characters=characters/eliza.character.json
