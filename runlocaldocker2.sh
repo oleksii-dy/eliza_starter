d53a8824ab80951be595c9ec4dabe9aa284786b2
mike dupont <mike.dupont@introspector.local>
2025-01-15 11:33:40 -0500

    update

--- runlocaldocker2.sh
+++ runlocaldocker2.sh
@@ -9,5 +9,5 @@
 #/usr/bin/docker run -p 3000:3000 --mount type=bind,source=/opt/agent,target=/opt/agent --env-file /var/run/agent/secrets/env  --rm --name "agent-docker.service" --entrypoint /opt/agent/docker-entrypoint-none.sh 767503528736.dkr.ecr.us-east-2.amazonaws.com/agent/eliza:feature-arm64_fastembed  /opt/agent/runlocaldocker-install-script.sh
 #/usr/bin/docker commit "agent-docker.service" "agent-docker-strace"
 # second step we debug with strace entrypoint
-/usr/bin/docker run -p 3000:3000 --mount type=bind,source=/opt/agent,target=/opt/agent --env-file /var/run/agent/secrets/env  --rm --name "agent-docker.service" --entrypoint /opt/agent/docker-entrypoint-strace2.sh 767503528736.dkr.ecr.us-east-2.amazonaws.com/agent/eliza:feature-arm64_fastembed  pnpm start:debug --characters=characters/eliza.character.json
+/usr/bin/docker run -p 3000:3000 --mount type=bind,source=/opt/agent,target=/opt/agent --env-file /var/run/agent/secrets/env  --rm --name "agent-docker.service" --entrypoint /opt/agent/docker-entrypoint-strace2.sh 767503528736.dkr.ecr.us-east-2.amazonaws.com/agent/eliza:feature-arm64_fastembed  
 #100755 >
