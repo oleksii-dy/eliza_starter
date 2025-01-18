9b92792339d910c4391885161e8bb4a229399f13
lispking <lispking@qq.com>
2025-01-18 11:31:26 +0800

    feat: support secp256k1/secp256r1 for sui

--- packages/plugin-sui/src/utils.ts
+++ packages/plugin-sui/src/utils.ts
@@ -1,14 +1,42 @@
 import { IAgentRuntime } from "@elizaos/core";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import { Secp256k1Keypair } from "@mysten/sui/keypairs/secp256k1";
+import { Secp256r1Keypair } from "@mysten/sui/keypairs/secp256r1";
 
-const parseAccount = (runtime: IAgentRuntime): Ed25519Keypair => {
+const parseAccount = (
+    runtime: IAgentRuntime
+): Ed25519Keypair | Secp256k1Keypair | Secp256r1Keypair => {
     const privateKey = runtime.getSetting("SUI_PRIVATE_KEY");
     if (!privateKey) {
         throw new Error("SUI_PRIVATE_KEY is not set");
     } else if (privateKey.startsWith("suiprivkey")) {
-        return Ed25519Keypair.fromSecretKey(privateKey);
+        return loadSecretKey(privateKey);
     } else {
-        return Ed25519Keypair.deriveKeypairFromSeed(privateKey);
+        return loadFromMnemonics(privateKey);
+    }
+};
+
+const loadSecretKey = (privateKey: string) => {
+    try {
+        return Ed25519Keypair.fromSecretKey(privateKey);
+    } catch {
+        try {
+            return Secp256k1Keypair.fromSecretKey(privateKey);
+        } catch {
+            return Secp256r1Keypair.fromSecretKey(privateKey);
+        }
+    }
+};
+
+const loadFromMnemonics = (mnemonics: string) => {
+    try {
+        return Ed25519Keypair.deriveKeypairFromSeed(mnemonics);
+    } catch {
+        try {
+            return Secp256k1Keypair.deriveKeypair(mnemonics);
+        } catch {
+            return Secp256r1Keypair.deriveKeypair(mnemonics);
+        }
     }
 };
 
