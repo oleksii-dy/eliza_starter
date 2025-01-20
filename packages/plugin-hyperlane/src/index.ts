cc904913fce542293dfde0985849f83bfee1c9ac
Suryansh-23 <suryanshchauhan2005@gmail.com>
2025-01-20 00:14:12 +0000

    feat: add 4th plugin boilerplate

--- packages/plugin-hyperlane/src/index.ts
+++ packages/plugin-hyperlane/src/index.ts
@@ -1,13 +1,16 @@
 import { assetTransferPlugin } from "./plugins/assetTransfer";
 import { crossChainMessagingPlugin } from "./plugins/crossChainMessaging";
+import { deployHyperlanePlugin } from "./plugins/deployHyperlane";
 import { deployWarpRoutePlugin } from "./plugins/deployWarpRoute";
 
 export const plugins = {
     crossChainMessagingPlugin,
     assetTransferPlugin,
     deployWarpRoutePlugin,
+    deployHyperlanePlugin,
 };
 
 export * from "./plugins/crossChainMessaging";
 export * from "./plugins/assetTransfer";
 export * from "./plugins/deployWarpRoute";
+export * from "./plugins/deployHyperlane";
