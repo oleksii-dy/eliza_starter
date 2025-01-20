38246bdb252a183311aac0859e9e4bd7dbfcf839
Duc LA <laminhduc2610@gmail.com>
2025-01-20 23:30:42 +0700

    update swap

--- packages/plugin-suimarket/src/actions/swapByAddress.ts
+++ packages/plugin-suimarket/src/actions/swapByAddress.ts
@@ -107,7 +107,7 @@ export const executeSwapByAddress: Action = {
         try {
 
             callback({
-               text:`Swap Tokens \n From: ${inputTokenObject.symbol} \n To:   ${outputTokenObject.symbol} \n Amount: ${content.amount} \n Please double-check all details before swapping to avoid any loss`,
+               text:`Swap Tokens \n From: ${inputTokenObject.symbol} \n To:   ${outputTokenObject.symbol}  \n Please double-check all details before swapping to avoid any loss`,
                action:"SUI_EXECUTE_SWAP_BY_ADDRESS",
                result: {
                 type: "swap",
