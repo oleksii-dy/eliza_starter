77f8b6b8156a2290504f1a9b5703a429dc3062d6
BitWonka <79578887+BitWonka@users.noreply.github.com>
2025-01-20 05:07:59 +0200

    update model to haiku
    updated to cheaper model haiku

--- packages/plugin-node/src/services/image.ts
+++ packages/plugin-node/src/services/image.ts
@@ -180,7 +180,7 @@ class AnthropicImageProvider implements ImageProvider {
             },
             body: JSON.stringify(
                 {
-                    model: "claude-3-opus-20240229",
+                    model: "claude-3-haiku-20240307",
                     max_tokens: 1024,
                     messages: [{role: "user", content}],
                 }),
