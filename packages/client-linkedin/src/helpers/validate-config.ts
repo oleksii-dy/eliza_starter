c120e4e33cc3b57e3cf6aaf70d16a0cdff2ca81c
stanislawkurzypBD <stanislaw.kurzyp@blockydevs.com>
2025-01-20 14:37:50 +0100

    Fixed cacheManager bug

--- packages/client-linkedin/src/helpers/validate-config.ts
+++ packages/client-linkedin/src/helpers/validate-config.ts
@@ -34,7 +34,7 @@ export const validateConfig = (runtime: IAgentRuntime) => {
     const LINKEDIN_ACCESS_TOKEN = runtime.getSetting("LINKEDIN_ACCESS_TOKEN");
     const LINKEDIN_POST_INTERVAL_MIN  = runtime.getSetting('LINKEDIN_POST_INTERVAL_MIN');
     const LINKEDIN_POST_INTERVAL_MAX  = runtime.getSetting("LINKEDIN_POST_INTERVAL_MAX");
-    const LINKEDIN_DRY_RUN  = runtime.getSetting("LINKEDIN_DRY_RUN");
+    const LINKEDIN_DRY_RUN  = runtime.getSetting("LINKEDIN_DRY_RUN") === "true" || false;
 
     try {
         const envs = configSchema.parse({
--- packages/client-linkedin/src/index.ts
+++ packages/client-linkedin/src/index.ts
@@ -20,7 +20,7 @@ export const LinkedInClient: Client = {
         const postManager = await PostsManager.create({
             axiosInstance,
             userInfoFetcher: new LinkedInUserInfoFetcher(axiosInstance),
-            runtime: this.runtime,
+            runtime,
             config: {
                 LINKEDIN_DRY_RUN: envs.LINKEDIN_DRY_RUN,
                 LINKEDIN_POST_INTERVAL_MIN: envs.LINKEDIN_POST_INTERVAL_MIN,
