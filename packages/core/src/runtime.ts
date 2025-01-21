ed6467e75240b09ce87c3fa05c1458d7bfbe16ef
Shawn Anderson <shawn@longtailfinancial.com>
2025-01-21 10:06:13 -0800

    Only get 5 post examples instead of 50.

--- packages/core/src/runtime.ts
+++ packages/core/src/runtime.ts
@@ -1095,7 +1095,7 @@ Text: ${attachment.text}
                 const messageString = `${post}`;
                 return messageString;
             })
-            .slice(0, 50)
+            .slice(0, 5)
             .join("\n");
 
         const formattedCharacterMessageExamples = this.character.messageExamples
