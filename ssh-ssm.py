c39f65aa56d9f7aa8066a09cf52151e568874854
mike dupont <mike.dupont@introspector.local>
2025-01-22 07:45:21 -0500

    hold for jump

--- ssh-ssm.py
+++ ssh-ssm.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python
 import time
-
+import json
 import boto3
 #from dateutil import tz
 
 
 def parse_command_id(send_command_output):
     return send_command_output['Command']['CommandId']
+
+def fwd(instance):
+    # https://aws.amazon.com/blogs/aws/new-port-forwarding-using-aws-system-manager-sessions-manager/
+    #INSTANCE_ID=$(aws ec2 describe-instances   --filter "Name=tag:Name,Values=CodeStack/NewsBlogInstance"          --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]"              --output text)
+# create the port forwarding tunnel
+    prms = {
+        "portNumber":["22"],
+        "localPortNumber":["2222"]
+    }
+    prms_jsn = json.dumps(prms)
+    print(f"""aws ssm start-session --target {instance} --document-name AWS-StartPortForwardingSession  --parameters '{prms_jsn}'""")
 
 def main():
     ec2_client = boto3.client('ec2')
@@ -26,8 +37,9 @@ def main():
             instance_id = instance['InstanceId']
             state = instance['State']['Name']            
             if state == 'running':
-                print(f"Starting command for instance: {instance_id}")    
-                print(f"aws ssm start-session --target  {instance_id}")
+                #print(f"Starting command for instance: {instance_id}")    
+                #print(f"aws ssm start-session --target  {instance_id}")
+                fwd(instance_id)
         
 if __name__ == "__main__":
     main()
