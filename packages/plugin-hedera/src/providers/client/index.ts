4a3728c7db0353aa1850a775a55c0d95db6dd90d
Stanislaw Kurzyp <stanislaw.kurzyp@arianelabs.com>
2025-01-23 12:40:41 +0100

    feat: wrap AgentKit in class, add eslint (CCS-14)
    Signed-off-by: Stanislaw Kurzyp <stanislaw.kurzyp@arianelabs.com>

--- packages/plugin-hedera/src/providers/client/index.ts
+++ packages/plugin-hedera/src/providers/client/index.ts
@@ -1,28 +1,43 @@
 import {elizaLogger, IAgentRuntime, Memory, State} from "@elizaos/core";
 import {HederaAgentKit} from "hedera-agent-kit";
 
-const initAgentKit = (_runtime: IAgentRuntime) => {
+
+export class HederaProvider {
+    private readonly agentKit: HederaAgentKit;
+
+    constructor(_runtime: IAgentRuntime) {
+        this.agentKit = initAgentKit(_runtime);
+    }
+
+    getHederaAgentKit(): HederaAgentKit {
+        return this.agentKit;
+    }
+}
+
+const initAgentKit = (_runtime: IAgentRuntime): HederaAgentKit => {
     const accountID = _runtime.getSetting("HEDERA_ACCOUNT_ID");
     const privateKey = _runtime.getSetting("HEDERA_PRIVATE_KEY");
     const networkType = _runtime.getSetting("HEDERA_NETWORK_TYPE") as "mainnet" | "testnet" | "previewnet";
+
     let hederaAgentKit: HederaAgentKit;
     try {
         hederaAgentKit = new HederaAgentKit(accountID, privateKey, networkType);
     } catch (error) {
-        elizaLogger.error('Error initialising HederaAgentKit: ',error);
+        elizaLogger.error('Error initialising HederaAgentKit: ', error);
     }
     return hederaAgentKit;
 }
 
 export const hederaClientProvider = {
-    async get (
+    async get(
         _runtime: IAgentRuntime,
         _message: Memory,
         state?: State
     ): Promise<string | null> {
         try {
-            const agentKit = initAgentKit(_runtime);
-            const balance = await agentKit.getHbarBalance();
+            const hederaProvider = new HederaProvider(_runtime);
+            const hederaAgentKit = hederaProvider.getHederaAgentKit();
+            const balance = await hederaAgentKit.getHbarBalance();
             const agentName = state?.agentName || "The agent";
             const address = _runtime.getSetting("HEDERA_ACCOUNT_ID");
 
