464cd1129af61bcbbbc51b8bcd00197d246773d0
Duc LA <laminhduc2610@gmail.com>
2025-01-24 18:31:02 +0700

    upate noed_moules

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-pnpm build && pnpm start --characters="characters/rockee_standard.json,characters/rockee_plus.json,characters/rockee_pro.json"
+pnpm build && pnpm start --characters="characters/rockee_standard.json"
