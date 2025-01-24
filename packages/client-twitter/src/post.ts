2ea33d7b53df7ee8e0ab7aa2e612a1d42fb00ffe
Indowebdeveloper <git@indowebdeveloper.com>
2025-01-24 22:13:28 +0700

    update

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -52,10 +52,9 @@ Your response must include the summary of the # NEWS above into a sentences and 
 If the news isnt about SUI or SUIRISE, dont mix the context with SUI Shilling. keep the original and relevant context of the news.
 Your response should be 1, 2, or 3 sentences (choose the length at random).
 Your response should not contain any questions. Brief, concise statements only. The total character count MUST be less than {{maxTweetLength}}. No emojis. Use \\n\\n (double spaces) between statements if there are multiple statements in your response.
-Replace any entities , person, organization name on your response with its twitter account handler ONLY if you know it, otherwise leave it as it is.
-Replace any crypto project name such as Ethereum, Bitcoin, or something else with its ticker starting with $.
-If you found any crypto project ticker that doesnt have $ before it you have to add the $ so for example ETH become $ETH.
-If there's any twitter handler already on the news you should keep it
+Your response should replace any entities , person, organization name on your response with its twitter account handler. Search from the web even if you think you know it and if you still cant find it just leave it as the original text.
+Your response should replace any crypto project name such as Ethereum, Bitcoin, or something else with its ticker starting with $.
+Your response should replace any crypto project ticker that doesnt have $ before it you have to add the $ so for example ETH become $ETH.
 `;
 
 export const twitterActionTemplate =
