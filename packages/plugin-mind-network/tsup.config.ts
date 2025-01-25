9a1ecc7e918ca2bd0197c82e343e91f6352e010c
Sayo <hi@sayo.wtf>
2025-01-25 22:40:24 +0530

    fix tests

--- packages/plugin-mind-network/tsup.config.ts
+++ packages/plugin-mind-network/tsup.config.ts
@@ -2,19 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts"],
-    outDir: "dist",
+    format: ["esm"],
+    dts: true,
+    splitting: false,
     sourcemap: true,
     clean: true,
-    format: ["esm"], 
-    external: [
-        "dotenv", 
-        "fs", 
-        "path", 
-        "@reflink/reflink",
-        "@node-llama-cpp",
-        "https",
-        "http",
-        "agentkeepalive",
-        "viem",
-    ],
 });
