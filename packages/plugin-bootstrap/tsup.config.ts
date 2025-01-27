1086c65c4bb7dd55d87af138777ba5d3a0d0251d
TULPtechnologies <carlo.dekker@tulp.app>
2025-01-27 16:00:20 +0100

    cool

--- packages/plugin-bootstrap/tsup.config.ts
+++ packages/plugin-bootstrap/tsup.config.ts
@@ -3,18 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig({
     entry: ["src/index.ts"],
     outDir: "dist",
+    format: ["esm"],
+    dts: true,
+    splitting: false,
     sourcemap: true,
     clean: true,
-    format: ["esm"], // Ensure you're targeting CommonJS
-    external: [
-        "dotenv", // Externalize dotenv to prevent bundling
-        "fs", // Externalize fs to use Node.js built-in module
-        "path", // Externalize other built-ins if necessary
-        "@reflink/reflink",
-        "@node-llama-cpp",
-        "https",
-        "http",
-        "agentkeepalive",
-        // Add other modules you want to externalize
-    ],
+    minify: false,
+    external: ["@elizaos/core", "whatwg-url"],
+    treeshake: true,
+    target: "node18",
 });
