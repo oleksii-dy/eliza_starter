e7571d489245fa97aeb957a92ca2b1fe9005bd35
Indowebdeveloper <git@indowebdeveloper.com>
2025-01-30 09:57:33 +0700

    OKOK

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -52,7 +52,6 @@ Your response must include the summary of the # NEWS above into a sentences and 
 If the news isnt about SUI or SUIRISE, dont mix the context with SUI Shilling. keep the original and relevant context of the news.
 Your response should be 1, 2, or 3 sentences (choose the length at random).
 Your response should not contain any questions. Brief, concise statements only. The total character count MUST be less than 400. No emojis. Use \n\n (double spaces) between statements if there are multiple statements in your response.
-Your response should replace any entities , person, organization name on your response with its twitter account handler.
 Your response should replace any crypto currency name such as Ethereum, Bitcoin, or something else with its ticker starting with $.
 Your response should replace any crypto currency ticker that doesnt have $ before it you have to add the $ so for example ETH become $ETH ( ticker has length up to 4 characters ).
 Your response should not include ChainCatcher text on it.
