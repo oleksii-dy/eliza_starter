c9b8d38defc41d5a0ff4000c2cd91b1c8ebac023
Indowebdeveloper <git@indowebdeveloper.com>
2025-01-31 13:33:18 +0700

    tpl

--- packages/plugin-crypto-news/src/templates/tweets.ts
+++ packages/plugin-crypto-news/src/templates/tweets.ts
@@ -14,10 +14,10 @@ export const twitterPostTemplate = `
 {{postDirections}}
 
 # Task: Generate a post in the voice and style and perspective of {{agentName}} @{{twitterUserName}}.
-Write a post that is {{adjective}} about the #NEWS, from the perspective of {{agentName}}. You are allowed to add opinion about the #NEWS, but it should be in the voice of {{agentName}}.
-If the news isnt about SUI or SUIRISE, dont mix the context with SUI Shilling. keep the original and relevant context of the news.
+Write a post that is {{adjective}} about the #NEWS, from the perspective of {{agentName}}. You must include the conclusion or summary of the NEWS, and you must include all important numbers, statistics and context of the NEWS.
+If the news isnt about SUI or SUIRISE, dont mix the context with SUI Shilling.
 Your response should be 1, 2 sentences (choose the length at random).
-Your response should not contain any questions. Brief, concise statements only. The total character count MUST be less than 280. No emojis. Use \n\n (double spaces) between statements if there are multiple statements in your response.
+Your response should not contain any questions. Brief, concise statements only. The total character count MUST be less than 400. No emojis. Use \n\n (double spaces) between statements if there are multiple statements in your response.
 Your response should replace any crypto currency name such as Ethereum, Bitcoin, or something else with its ticker starting with $.
 Your response should replace any crypto currency ticker that doesnt have $ before it you have to add the $ so for example ETH become $ETH ( ticker has length up to 4 characters ).
 Your response should not include ChainCatcher text on it.
