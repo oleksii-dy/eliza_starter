e96deca7804503f5ddf1a28adc4bd374eb0c0c57
Marvin Arnold <marvin@geeky.rocks>
2025-02-02 21:56:03 -0600

    feat: mintToken

--- packages/plugin-twas/src/index.ts
+++ packages/plugin-twas/src/index.ts
@@ -2,12 +2,13 @@ import type { Plugin } from "@elizaos/core";
 import { GenerateListingAction } from "./actions/postListing";
 import { ProposeListing } from "./actions/brainstormListing";
 import { EchoAction } from "./actions/echo";
+import { MintTokenAction } from "./actions/mintToken";
 
 // Action setup
 export const twasPlugin: Plugin = {
     name: "twas",
     description: "Twas Protocol Plugin for Eliza",
-    actions: [GenerateListingAction, ProposeListing, EchoAction],
+    actions: [GenerateListingAction, ProposeListing, MintTokenAction, EchoAction],
     evaluators: [],
     providers: []
 };
