7bc3c8fa42126ee3adec259cfb613e5e72ca184d
ben-dh3 <benjaminduffield1997@gmail.com>
2025-02-04 10:43:48 +0000

    add current news action

--- packages/plugin-test/src/index.ts
+++ packages/plugin-test/src/index.ts
@@ -1,13 +1,13 @@
 import type { Plugin } from "@elizaos/core";
 import { helloWorldAction } from "./actions/helloworld.ts";
-
+import { currentNewsAction } from "./actions/currentNews.ts";
 export * as evaluators from "./evaluators/index.ts";
 export * as providers from "./providers/index.ts";
 
 export const testPlugin: Plugin = {
     name: "test",
     description: "Test plugin with basic actions and evaluators",
-    actions: [helloWorldAction],
+    actions: [helloWorldAction, currentNewsAction],
 };
 
 export default testPlugin;
