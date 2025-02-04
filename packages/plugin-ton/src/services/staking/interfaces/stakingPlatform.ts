97c81903aedea9fd54a13569f5b463276d4ced10
tylermcwilliams <tyler.ellis.mcwilliams@gmail.com>
2025-02-04 20:29:51 +0100

    chore: changed platform strategy interface to return ton MessageRelaxed

--- packages/plugin-ton/src/services/staking/interfaces/stakingPlatform.ts
+++ packages/plugin-ton/src/services/staking/interfaces/stakingPlatform.ts
@@ -1,7 +1,7 @@
-import { TonClient } from "@ton/ton";
+import { MessageRelaxed, TonClient } from "@ton/ton";
 
 export interface StakingPlatform {
   getPoolInfo(client: TonClient, poolAddress: string): Promise<any>;
-  stake(client: TonClient, poolAddress: string, amount: number): Promise<string | null>;
-  unstake(client: TonClient, poolAddress: string, amount: number): Promise<string | null>;
+  createStakeMessage(client: TonClient, poolAddress: string, amount: number): Promise<MessageRelaxed>;
+  createUnstakeMessage(client: TonClient, poolAddress: string, amount: number): Promise<MessageRelaxed>;
 }
