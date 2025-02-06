543866fb7810df482c986cc9317953b337275bbb
yuyuding <wangchaoxyxy@126.com>
2025-02-07 02:43:23 +0800

    add-vana

---
+++ packages/plugin-vana/src/index.ts
@@ -0,0 +1,19 @@
+export * from "./actions/transfer";
+export * from "./providers/wallet";
+export * from "./types";
+
+import type { Plugin } from "@elizaos/core";
+// import { transferAction } from "./actions/transfer";
+// import { vanaWalletProvider } from "./providers/wallet";
+
+export const vanaPlugin: Plugin = {
+    name: "vana",
+    description:
+        "It's Vana!",
+    providers: [],//vanaWalletProvider
+    evaluators: [],
+    services: [],
+    actions: [],//transferAction
+};
+
+export default vanaPlugin;
