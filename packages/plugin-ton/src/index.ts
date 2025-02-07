c752b87fdbcecee8d90b30bd96125b0710dc343a
anuragasawa20 <img_2021012@iiitm.ac.in>
2025-02-08 01:10:51 +0530

    token price fetch action pushed

--- packages/plugin-ton/src/index.ts
+++ packages/plugin-ton/src/index.ts
@@ -1,15 +1,18 @@
 import type { Plugin } from "@elizaos/core";
 import transferAction from "./actions/transfer.ts";
 import { WalletProvider, nativeWalletProvider } from "./providers/wallet.ts";
+import tokenPriceAction from "./actions/tokenPrice.ts";
+import { tonTokenPriceProvider } from "./providers/tokenProvider.ts";
 
 export { WalletProvider, transferAction as TransferTonToken };
+export { tokenPriceAction as GetTokenPrice };
 
 export const tonPlugin: Plugin = {
-    name: "ton",
-    description: "Ton Plugin for Eliza",
-    actions: [transferAction],
-    evaluators: [],
-    providers: [nativeWalletProvider],
+  name: "ton",
+  description: "Ton Plugin for Eliza",
+  actions: [transferAction, tokenPriceAction],
+  evaluators: [],
+  providers: [nativeWalletProvider, tonTokenPriceProvider],
 };
 
 export default tonPlugin;
