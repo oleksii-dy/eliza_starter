a314c48930fe78d1e2cd2a4095286abcd77f6e56
Sayo <hi@sayo.wtf>
2025-02-13 19:14:02 +0530

    add todo flags
    added todo flag

--- packages/plugin-anthropic/src/index.ts
+++ packages/plugin-anthropic/src/index.ts
@@ -85,6 +85,9 @@ export const anthropicPlugin: Plugin = {
       return text;
     },
     [ModelClass.TEXT_EMBEDDING]: async (runtime, text: string | null) => {
+
+      // TODO: Make this fallback only!!!
+      // TODO: pass on cacheDir to FlagEmbedding.init
       if (!text) return new Array(1536).fill(0);
       
       const model = await FlagEmbedding.init({ model: EmbeddingModel.BGESmallENV15 });
