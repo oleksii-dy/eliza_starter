8597f2d624914246cbf777550dbe7ec7b9e462b2
Lovel George <36924734+lovelgeorge99@users.noreply.github.com>
2025-02-14 08:55:43 -0500

    added actions in qacc-plugin

--- packages/plugin-qacc/src/index.ts
+++ packages/plugin-qacc/src/index.ts
@@ -1,34 +1,12 @@
 import type { Plugin } from "@elizaos/core";
-import { continueAction } from "./actions/continue.ts";
-import { followRoomAction } from "./actions/followRoom.ts";
-import { ignoreAction } from "./actions/ignore.ts";
-import { muteRoomAction } from "./actions/muteRoom.ts";
-import { noneAction } from "./actions/none.ts";
-import { unfollowRoomAction } from "./actions/unfollowRoom.ts";
-import { unmuteRoomAction } from "./actions/unmuteRoom.ts";
-import { factEvaluator } from "./evaluators/fact.ts";
-import { goalEvaluator } from "./evaluators/goal.ts";
-import { boredomProvider } from "./providers/boredom.ts";
-import { factsProvider } from "./providers/facts.ts";
-import { timeProvider } from "./providers/time.ts";
 
 export * as actions from "./actions";
-export * as evaluators from "./evaluators";
-export * as providers from "./providers";
 
-export const bootstrapPlugin: Plugin = {
-    name: "bootstrap",
+export const qaccPlugin: Plugin = {
+    name: "qacc",
     description: "Agent bootstrap with basic actions and evaluators",
-    actions: [
-        continueAction,
-        followRoomAction,
-        unfollowRoomAction,
-        ignoreAction,
-        noneAction,
-        muteRoomAction,
-        unmuteRoomAction,
-    ],
-    evaluators: [factEvaluator, goalEvaluator],
-    providers: [boredomProvider, timeProvider, factsProvider],
+    actions: [],
+    evaluators: [],
+    providers: [],
 };
-export default bootstrapPlugin;
+export default qaccPlugin;
