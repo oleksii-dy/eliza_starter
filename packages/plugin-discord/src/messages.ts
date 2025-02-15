f813e0a1c28431f61186f93a4bf72e75e2f5517a
0xbbjoker <0xbbjoker@proton.me>
2025-02-16 00:45:19 +0100

    fix: save discord msg response

--- packages/plugin-discord/src/messages.ts
+++ packages/plugin-discord/src/messages.ts
@@ -305,7 +305,7 @@ export class MessageManager {
 
             await this.runtime.databaseAdapter.log({
               body: { message, context, response: responseContent },
-              userId: userId,
+              userId: this.runtime.agentId,
               roomId,
               type: "response",
             });
