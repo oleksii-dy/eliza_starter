bbd1cf30270485465787e1e32e0335e98ff90b08
emmmm <155267286+eeemmmmmm@users.noreply.github.com>
2025-02-16 15:30:56 +0100

    Update prompts.ts

--- packages/client-farcaster/src/prompts.ts
+++ packages/client-farcaster/src/prompts.ts
@@ -7,7 +7,7 @@ import type { Cast } from "./types";
 
 export const formatCast = (cast: Cast) => {
     return `ID: ${cast.hash}
-    From: ${cast.profile.name} (@${cast.profile.username})${cast.profile.username})${cast.inReplyTo ? `\nIn reply to: ${cast.inReplyTo.fid}` : ""}
+    From: ${cast.profile.name} (@${cast.profile.username})${cast.inReplyTo ? `\nIn reply to: ${cast.inReplyTo.fid}` : ""}
 Text: ${cast.text}`;
 };
 
@@ -40,7 +40,7 @@ export const postTemplate =
     `
 # Task: Generate a post in the voice and style of {{agentName}}, aka @{{farcasterUsername}}
 Write a single sentence post that is {{adjective}} about {{topic}} (without mentioning {{topic}} directly), from the perspective of {{agentName}}.
-Try to write something totally different than previous posts. Do not add commentary or ackwowledge this request, just write the post.
+Try to write something totally different than previous posts. Do not add commentary or acknowledge this request, just write the post.
 
 Your response should not contain any questions. Brief, concise statements only. No emojis. Use \\n\\n (double spaces) between statements.`;
 
