74c46807e4929cc419b246fd3c52304dbbfc122c
0xbbjoker <0xbbjoker@proton.me>
2025-03-14 01:23:03 +0100

    feat: enhance static migrations

--- packages/plugin-sql/drizzle.config.ts
+++ packages/plugin-sql/drizzle.config.ts
@@ -1,14 +1,33 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+import os from "node:os";
+import path from "node:path";
 
 config({ path: "../../.env" });
 
-export default defineConfig({
-	dialect: "postgresql",
-	schema: "./src/schema/index.ts",
-	out: "./drizzle/migrations",
-	dbCredentials: {
-		url: process.env.POSTGRES_URL || "file://../../pglite",
-	},
-	breakpoints: true,
-});
+const homeDir = os.homedir();
+const elizaDir = path.join(homeDir, ".eliza");
+const elizaDbDir = path.join(elizaDir, "db");
+const pgliteUrl = `file:///${elizaDbDir}`;
+
+const drizzleConfig = process.env.POSTGRES_URL
+	? {
+			dialect: "postgresql" as const,
+			schema: "./src/schema/index.ts",
+			out: "./drizzle/migrations",
+			dbCredentials: {
+				url: process.env.POSTGRES_URL,
+			},
+			breakpoints: true,
+	  }
+	: {
+			dialect: "postgresql" as const,
+			driver: "pglite",
+			schema: "./src/schema/index.ts",
+			out: "./drizzle/migrations",
+			dbCredentials: {
+				url: pgliteUrl,
+			},
+			breakpoints: true,
+	  };
+export default defineConfig(drizzleConfig);
