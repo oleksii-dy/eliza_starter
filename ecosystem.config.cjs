94d60472136b2265d26ef97b124934271b3f7ced
Toky Ralala <ralala.toky@gmail.com>
2025-07-10 12:06:38 -0400

    fix ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'eliza-agent',
-      cwd: '/var/www/eliza',
+      cwd: '/var/www/eliza-evo',
       script: 'elizaos',
       args: 'start --character="characters/evolucia.character.json"',
       env: {
@@ -12,18 +12,6 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '3G',
-    },
-    // {
-    //   name: 'eliza-client',
-    //   cwd: '/var/www/eliza',
-    //   script: 'pnpm',
-    //   args: 'start:client --port 5173',
-    //   env: {
-    //     VITE_SERVER_BASE_URL: 'https://api.evolucia.com'
-    //   },
-    //   autorestart: true,
-    //   watch: false,
-    //   max_memory_restart: '1G',
-    // }
+    }
   ]
 };
