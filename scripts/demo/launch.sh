4515855c598385f14238a2621eac0a716fe2a94e
Toky Ralala <ralala.toky@gmail.com>
2025-07-10 13:37:15 -0400

    fix demo script

--- scripts/demo/launch.sh
+++ scripts/demo/launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd packages/client-telegram
+cd packages/plugin-telegram
 git pull origin wau/main
 
 cd ../../
 
 bun clean:demo && git restore bun.lock && bun install && bun run build
 
-pm2 reload ecosystem.config.js
+pm2 reload ecosystem.config.cjs
 pm2 save
